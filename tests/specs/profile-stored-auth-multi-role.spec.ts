3a5a66b1d273c83c01a9b69326f83e7322cb34f4
ccdita <christaldita@gmail.com>
2026-05-19 12:35:39 -0400

    Implement problem user auth in single test
    Implement the problem user authentication in the same test that verifies
    standard user authentication.

--- tests/specs/profile-stored-auth-multi-role.spec.ts
+++ tests/specs/profile-stored-auth-multi-role.spec.ts
@@ -14,7 +14,14 @@ let loginPage: LoginPage;
 
 test.describe('Swag Labs - Profiles', () => {
 
-    test('Multiple roles', async ({ browser }) => {
+    /**
+     * Why are there no tests for the locked out and nonexistent users?
+     * - These tests assume that auth setup has already run, and require an authentication state to
+     * restore and validate the login status
+     * - Authentication tests for the locked out and nonexistent users are already handled within the
+     * auth setup file itself
+     */
+    test('Standard User and Problem User', async ({ browser }) => {
         // ----- STANDARD USER -----
         /**
          * Create a new browser context, which is akin to a separate browser session with its own:
@@ -28,6 +35,13 @@ test.describe('Swag Labs - Profiles', () => {
         const standardUserPage = await standardUserContext.newPage(); // Create a new tab/page within the browser
         await standardUserPage.goto('/inventory.html');
         loginPage = new LoginPage(standardUserPage); // Pass authenticated page into POM
+        await loginPage.checkLoggedIn();
+
+        // ----- PROBLEM USER -----
+        const problemUserContext = await browser.newContext({ storageState: '.auth/problem_user.json' });
+        const problemUserPage = await problemUserContext.newPage();
+        await problemUserPage.goto('/inventory.html');
+        loginPage = new LoginPage(problemUserPage);
         await loginPage.checkLoggedIn();
     });
 });
