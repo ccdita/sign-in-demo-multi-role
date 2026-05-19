e2c456248e342e6eab1fba23dfcb50162e34189b
ccdita <christaldita@gmail.com>
2026-05-18 22:59:52 -0400

    Update Playwright configuration
    Add a custom test ID attribute since Swag Labs uses the attribute
    'data-test'. Turn tracing on at all times.

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,9 +27,10 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://www.saucedemo.com/',
+    testIdAttribute: 'data-test',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
     screenshot: 'only-on-failure',
   },
 
